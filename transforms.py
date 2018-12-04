ce9cf2fa25b2343d50d60ca0f6986718f7eaf57a
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2018-12-04 12:03:30 -0500

    translation stuff

--- transforms.py
+++ transforms.py
@@ -2,7 +2,7 @@ from preprocessing  import process_embedding
 from preprocessing  import check_valid_file
 from preprocessing  import check_valid_dir
 
-import numpy                        as np
+import numpy as np
 
 from progressbar    import progressbar
 from tqdm           import tqdm
@@ -34,8 +34,36 @@ def parse_args():
 # RETURNS: transformed matrix. 
 
 def transflow():
+     
+    check_valid_file(emb_path)
+    if os.path.isfile(out_path):
+        print("There is already a matrix saved with this name. ")
+        exit() 
+
+    # Take the first $n$ most frequent word vectors for a subset. 
+    # Set to 0 to take entire embedding. 
+    first_n = 0
+   
+    vectors_matrix,label_df = process_embedding(emb_path,first_n,None)
+    num_rows = len(vectors_matrix)
+
+    sample_vector = vectors_matrix[0]
+    dimensions = len(sample_vector)
+
+    # Generate the translation unit vector. 
+    translation_vec = [1]
+    for j in range(1, dimensions):
+        translation_vec.append(0)
     
+    translation_size = 6
 
+    trans_matrix = []
 
-
+    for j in range(0, dimensions):
+        translation_vec[j] = translation_size * translation_vec[j]
+    
+    for i in range(0, numrows):
+        for j in range(0, dimensions):
+            trans_matrix[i][j] = vectors_matrix[i][j] + translation_vec[j]
+    
 
