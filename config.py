16ca7aaf62a9bc16a71120b22c6e3c3334cb0257
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-04 16:25:22 -0500

    params

--- config.py
+++ config.py
@@ -1,3 +1,23 @@
+import affine_funcs
+import numpy as np
+import math
+
 # PARAMETERS FOR TRANFORMATIONS
 
+def get_config(dim):
+    transforms = []
+    
+    # Argument list for translation transform. 
+    # FORMAT: [ direction_vector, magnitude ]
+    translation_args = []
+    print(np.array([1 if i == 0 else 0 for i in range(dim)]))
+    translation_args.append([ np.ones(dim) / math.sqrt(dim), 1 ])
+    translation_args.append([ np.zeros(dim) / math.sqrt(dim), 1 ])
 
+
+
+    transforms.append([ affine_funcs.translation, np.ones(dim), 1])
+
+
+if __name__ == '__main__':
+    get_config(100)
