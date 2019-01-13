f407ecec4e9190b8e33e2fcd9fa1bb0c61d48798
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-13 15:21:00 -0500

    Bugfix missing last join char char

---
+++ extension_swap.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+target_list_path = sys.argv[1]
+new_ext = 'bin'
+
+with open(target_list_path) as f:
+    target_list = f.readlines()
+
+for i,target in enumerate(target_list):
+    target_list[i] = os.path.abspath(target)
+    target = os.path.abspath(target)
+    extension = target.split('.')[-1]
+    new_targ = '.'.join(target.split('.')[:len(target.split('.')) - 1]) + '.' + new_ext
+    print(new_targ)
