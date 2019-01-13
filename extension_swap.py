db386aea69cd19d8c4b3dac788d9e52770699e6c
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-13 15:26:06 -0500

    Bugfix newline errant

--- extension_swap.py
+++ extension_swap.py
@@ -9,6 +9,9 @@ with open(target_list_path) as f:
 
 for i,target in enumerate(target_list):
     target = os.path.abspath(target)
+    target = list(target)
+    target.remove('\n')
+    target = "".join(target)
     extension = target.split('.')[-1]
     new_targ = '.'.join(target.split('.')[:len(target.split('.')) - 1]) + '.' + new_ext
     print(target)
