8d053f2cca6bfcd30cc80022352ee499cafcee7b
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-01-13 15:22:40 -0500

    Implemented system move

--- extension_swap.py
+++ extension_swap.py
@@ -8,8 +8,7 @@ with open(target_list_path) as f:
     target_list = f.readlines()
 
 for i,target in enumerate(target_list):
-    target_list[i] = os.path.abspath(target)
     target = os.path.abspath(target)
     extension = target.split('.')[-1]
     new_targ = '.'.join(target.split('.')[:len(target.split('.')) - 1]) + '.' + new_ext
-    print(new_targ)
+    os.system('mv' + ' ' + target + ' ' + new_targ)
